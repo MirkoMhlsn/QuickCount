2c7b102354b556c45e792ffd8b592ac814c6d73b
Mirko Mühlhausen <mirko.muehlhausen@stud.fh-rosenheim.de>
2020-08-17 01:37:27 +0200

    addedexercise activity and corresponding xml, removed old junk

---
+++ QuickCount/app/src/main/java/android/example/quickcount/ui/ExerciseActivity.java
@@ -0,0 +1,16 @@
+package android.example.quickcount.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.example.quickcount.R;
+import android.os.Bundle;
+
+public class ExerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercise_activity);
+
+    }
+}
